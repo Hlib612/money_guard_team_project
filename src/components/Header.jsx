4d1fed0ab8c7b87e9be83bf19b6524bb0af11551
Hlib <glebmalonos@gmail.com>
2026-06-17 22:20:08 +0300

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,25 @@
 // nут має бути імпорт з форми регістра де import name from './FormRegister.jsx'
 import css from '../css_modules/Header.module.css'
-// import logo1x from './assets/images/logo_1x.png'
-//alt
-//Stylized gold shield above white chevrons
-// Logout icon — white rectangle with right-pointing arrow"
+import logo1x from '../images/logo_1x.png'
+import logo2x from '../images/logo_2x.png'
+import exit1x from '../images/exit_1x.png'
+import exit2x from '../images/exit_2x.png'
+
+// Logout icon — white rectangle with right-pointing arrow"  srcSet='logo1x 1x, logo2x 2x'
 export default function Header(){
 
     return(
         <>
         <header className={css.header}>
             <div className={css.logo_div}>
-                <img src="#" alt="#" />
-                <p>Money Guard</p>
+                <img className={css.logo_img} src={logo1x} alt="Stylized gold shield above white chevrons" srcSet={`${logo1x} 1x, ${logo2x} 2x`} />
+                <p className={css.logo_text}>Money Guard</p>
             </div>
             <div className={css.big_exit_div}>
-                <p>Name</p>
+                <p className={css.exit_text}>Name</p>
                 <div className={css.exit_div}>
-                    <img src="#" alt="#"/>
-                    <p>Exit</p>
+                    <img src={exit1x} alt="#" srcSet={`${exit1x} 1x, ${exit2x} 2x`}/>
+                    <p className={css.exit_text}>Exit</p>
                 </div>
             </div>
         </header>
