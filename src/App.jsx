a28f21e7009f44d55693665d2de04e9da2e23f54
Kira <gajdukkira232@gmail.com>
2026-07-08 14:19:54 +0300

    logout-register-login-Forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import Header from "./components/header"
-import Home from "./components/Home"
-// import './App.css'
+import { useState } from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+  const [userName, setUserName] = useState("");
 
-  return (
-    <>
-    <Header/>
-    <Home/>
-    </>
-  )
+  if (isLoggedIn) {
+    return (
+      <>
+        <Header
+          userName={userName}
+          onLogout={() => {
+            setIsLoggedIn(false);
+            setShowRegister(false);
+            setUserName("");
+          }}
+        />
+        <Home />
+      </>
+    );
+  }
+
+  return showRegister ? (
+    <RegisterForm
+      onBack={() => setShowRegister(false)}
+      onRegister={(name) => {
+        setUserName(name);
+        setIsLoggedIn(true);
+      }}
+    />
+  ) : (
+    <LoginForm
+      onLogin={() => setIsLoggedIn(true)}
+      onRegister={() => setShowRegister(true)}
+    />
+  );
 }
 
-export default App
+export default App;
