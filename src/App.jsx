f769f4d4bab80be4563585b20c849fa3a7c2e96a
Hlib <glebmalonos@gmail.com>
2026-07-08 14:25:08 +0300

    add react router logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Sidebar from "./components/Sidebar/Sidebar"
 import Statistics from "./components/Statistics/Statistics"
 import styles from "./css_modules/app.module.css"
 import Home from "./components/Home"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 function App() {
   return (
+    <BrowserRouter basename="/money_guard_team_project">
     <div className={styles.appBg}>
       <div className={styles.extraShadow} />
       <div className={styles.pageWrapper}>
@@ -12,12 +14,17 @@ function App() {
         <div className={styles.layout}>
           <Sidebar />
           <main className={styles.main}>
-            <Statistics />
-            <Home/>
+            <Routes>
+          {/* Редірект за замовчуванням на /statistics */}
+          <Route path="/" element={<Navigate replace to="/statistics" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/statistics" element={<Statistics />} />
+        </Routes>
           </main>
         </div>
       </div>
     </div>
+    </BrowserRouter>
   )
 }
 
